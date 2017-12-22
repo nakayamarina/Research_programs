3722399e8fa149ab1d086615ccfcd5f66d7b97be
中山 梨奈 <nakayamarina@chuuzan-no-iMac-6.local>
2017-12-22 22:11:01 +0900

    For automatic execution

--- run.sh
+++ run.sh
@@ -1,3 +1,36 @@
-Rscript TDAvec_autocor.r ../tameshi/20170130ar/12ch/RawData/
-Rscript TDAvec_autocor.r ../tameshi/20170130ar/mb/RawData/
-Rscript TDAvec_autocor.r ../tameshi/20170130ar/mb/MAL5/
+PATH_DATA="../Data_block/"
+
+DIRs=`ls -F ${PATH_DATA} | grep /`
+
+for dir in $DIRs
+do
+
+  for image_method in 12ch 32ch mb
+  do
+
+    PATH_voxel="${PATH_DATA}${dir}${image_method}/"
+
+    echo "------------ ${PATH_voxel} ---------------"
+    python Preprocessing_block.py ${PATH_voxel}
+
+
+    PATH_RAW="${PATH_DATA}${dir}${image_method}/RawData/"
+
+    echo "------------ ${PATH_RAW} ---------------"
+    python MovingAverageLine.py ${PATH_RAW}
+
+    python TAUautocor.py ${PATH_RAW}
+
+    Rscript TDAvec_autocor.r ${PATH_RAW}
+
+
+    PATH_MAL="${PATH_DATA}${dir}${image_method}/MAL5/"
+
+    echo "------------ ${PATH_MAL} ---------------"
+    python TAUautocor.py ${PATH_MAL}
+
+    Rscript TDAvec_autocor.r ${PATH_MAL}
+
+  done
+
+done
