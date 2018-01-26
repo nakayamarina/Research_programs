fc6fe7246648bf55ad0df74ff54000357ff45768
nakayamarina <r.nakayama.kut@gmail.com>
2018-01-27 05:19:18 +0900

    Correction of commentout

--- SVM_Raw.py
+++ SVM_Raw.py
@@ -4,25 +4,25 @@
 # # SVMによる学習と識別性能評価（生データ）
 
 # ---
-#   
-# 引数：raw_tap.csv/raw_rest.csvがあるディレクトリまでのパス  
-#   
+#
+# 引数：raw_tap.csv/raw_rest.csvがあるディレクトリまでのパス
+#
 # ---
-#   
-# 入力： 
+#
+# 入力：
 # * raw_tap.csv
 # * raw_rest.csv
-# 
+#
 # ---
-#   
-# 出力：ACCURACY[loo][k_list]_Raw.csv　識別性能評価結果一覧  
+#
+# 出力：ACCURACY[loo][k_list]_Raw.csv　識別性能評価結果一覧
 # k_listはk-分割交差検証法で用いた分割数
-# 
-# ---  
-# 
+#
+# ---
+#
 # 生データをSVMを用いて学習し，交差検証法（k-分割交差検証，leave-one-out交差検証）を用いて識別性能評価を行う．
-# 
-# 
+#
+#
 
 # In[1]:
 
@@ -39,11 +39,11 @@ from sklearn.model_selection import train_test_split
 
 # In[2]:
 
-#args = sys.argv
-#PATH = args[1]
+args = sys.argv
+PATH = args[1]
 
 # jupyter notebookのときはここで指定
-PATH = '../Data_block/20170130ar/12ch/RawData/'
+#PATH = '../Data_block/20170130ar/12ch/RawData/'
 
 
 # In[3]:
@@ -57,9 +57,9 @@ k_list = [20]
 
 
 # ## SVM_LOO関数
-# 引数としてTrainingData関数で作成した教師データをX，ラベルをyで受け取る．  
+# 引数としてTrainingData関数で作成した教師データをX，ラベルをyで受け取る．
 # 交差検証法の一つleave-one-out交差検証で識別精度評価を行う．
-# 
+#
 # * (1個をテストデータ，残りを教師データにして学習・評価) * すべてのデータ個
 # * 得られたすべてのデータ個の評価結果（識別率）の平均を求めてパーセントに直す
 # * 評価結果（識別率）をTrainingData関数に返す
@@ -69,7 +69,7 @@ k_list = [20]
 def SVM_LOO(X, y):
 
     LOOscore = np.zeros(len(X))
-    
+
     # 1個をテストデータ，残りを教師データにして学習・評価
     # すべてのデータに対して行う
     for i in range(len(X)):
@@ -84,29 +84,29 @@ def SVM_LOO(X, y):
 
         # 線形SVMのインスタンスを生成
         model = svm.SVC(kernel = 'linear', C = 1)
-        
+
         # モデルの学習
         model.fit(X_train, y_train)
 
         # 評価結果（識別率）を格納
         LOOscore[i] = model.score(X_test, y_test)
 
-        
+
     # 評価結果（識別率）の平均を求める
     result = LOOscore.mean()
-    
+
     # パーセントに直す
     result = round(result * 100, 1)
-    
+
     print(str(LOOscore) + '\n')
 
     return result
 
 
 # ## SVM_kCV関数
-# 引数としてTrainingData関数で作成した教師データをX，ラベルをy，データ分割数をkで受け取る．  
+# 引数としてTrainingData関数で作成した教師データをX，ラベルをy，データ分割数をkで受け取る．
 # 交差検証法の一つk-分割交差検証で識別精度評価を行う．
-# 
+#
 # * 学習
 # * (k分割し，1グループをテストデータ，残りグループを教師データにして評価) * k
 # * 得られたk個の評価結果（識別率）の平均を求めてパーセントに直す
@@ -115,29 +115,29 @@ def SVM_LOO(X, y):
 # In[5]:
 
 def SVM_kCV(X, y, k):
-    
+
     # 線形SVMのインスタンスを生成
     model = svm.SVC(kernel = 'linear', C = 1)
-    
+
     # k分割し，1グループをテストデータ，残りグループを教師データにして評価
     # すべてのグループに対して行う
     # 評価結果（識別率）を格納
     CVscore = cross_validation.cross_val_score(model, X, y, cv = k)
-    
+
     # 評価結果（識別率）の平均を求める
     result = CVscore.mean()
-    
+
     # パーセントに直す
     result = round(result * 100, 1)
 
     print('k = ' + str(k) + '：' + str(CVscore))
-    
+
     return result
-    
+
 
 
 # ## TrainingData関数
-# 引数として読み込みたいTapping/Restのそれぞれのファイル名をfile_tap/file_restで受け取る． 
+# 引数として読み込みたいTapping/Restのそれぞれのファイル名をfile_tap/file_restで受け取る．
 # * 機械学習にかけれるようにデータのベクトル化とラベルを作成
 # * データとラベルをSVM_LOO関数，SVM_kCV関数に渡す
 # * 帰ってきた識別率をまとめてmain関数に返す
@@ -145,14 +145,14 @@ def SVM_kCV(X, y, k):
 # In[6]:
 
 def TrainingData(file_rest, file_tap):
-    
+
     # 読み込みたいファイルのパス
     PATH_rest = PATH + file_rest
     PATH_tap = PATH + file_tap
 
     # csvファイル読み込み
     rest = pd.read_csv(PATH_rest, header = 0)
-    tap = pd.read_csv(PATH_tap, header = 0) 
+    tap = pd.read_csv(PATH_tap, header = 0)
 
     # RestとTappingのデータをまとめる
     all_data = pd.concat([rest, tap], axis = 0)
@@ -166,43 +166,43 @@ def TrainingData(file_rest, file_tap):
 
     y = np.r_[label_rest, label_tap]
 
-    
+
     # 学習とleave-one-out交差検証
-    
+
     print('leave-one-out')
 
     col_name = 'leave-one-out'
-    
+
     #print(col_name)
-    
+
     # SVM_LOO関数
     result_LOO = SVM_LOO(X, y)
-    
+
     # 評価結果（識別率）をデータフレームに変換・格納
     results = pd.DataFrame({col_name : [result_LOO] })
-    
-    
+
+
     # 学習とk-分割交差検証
-    
+
     print('k-hold Cross-Validation')
-    
+
     # k_listに従って学習・評価
     for i in k_list:
-        
+
         col_name = 'k = ' + str(i)
-        
+
         #print(col_name)
-        
+
         # SVM_CV関数
         result_CV = SVM_kCV(X, y, i)
-        
+
         # 評価結果（識別率）をデータフレームに変換・格納
         result_CV = pd.DataFrame({col_name : [result_CV] })
         results = pd.concat([results, result_CV], axis = 1)
-    
+
     return results
 
-    
+
 
 
 # ## main関数
@@ -210,7 +210,7 @@ def TrainingData(file_rest, file_tap):
 # In[7]:
 
 if __name__ == '__main__':
-    
+
     # 生データの識別率
 
     print('--------- ' + DATA_NAME + ' ---------')
@@ -219,13 +219,10 @@ if __name__ == '__main__':
 
     # インデックス名をつける
     raw_result.index = [DATA_NAME]
-    
+
     # csv書き出し
     PATH_RESULT = PATH + 'ACCURACY[loo]' + str(k_list) + '_Raw.csv'
     raw_result.to_csv(PATH_RESULT, index = True)
 
 
 # In[ ]:
-
-
-
