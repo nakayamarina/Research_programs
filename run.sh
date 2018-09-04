9fd77fc38a900310806e1a8cbe8aeaf3cfce3c7e
nakayamarina <r.nakayama.kut@gmail.com>
2018-09-05 08:20:48 +0900

    etc

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-PATH_DATA="../Data_block/analysis_by_programs/"
+PATH_DATA="../Data_state/analysis_by_programs/"
 
 DIRs=`ls -F ${PATH_DATA} | grep /`
 
@@ -25,7 +25,46 @@ do
 
     # python RAWvec_SPMts.py ${PATH_RAW}
 
-    python SVM_RAWvec_SPMts.py ${PATH_RAW}
+    # python SVM_RAWvec_SPMts.py ${PATH_RAW}
+
+    Rscript TDAvec_autocor_custom.r ${PATH_RAW}
+
+    python ReVectrize_ProposedMethod.py ${PATH_RAW}
+
+
+
+    echo "------------ python ML_SVM_RAWts.py ---------------"
+    python ML_SVM_RAWts.py ${PATH_RAW}
+
+    echo "------------ python ML_1dCNN_RAWts.py ---------------"
+    python ML_1dCNN_RAWts.py ${PATH_RAW}
+
+    echo "------------ python ML_SVM_ProposedMethod4.py 300 01---------------"
+    python ML_SVM_ProposedMethod4.py ${PATH_RAW}
+
+    echo "------------ python ML_SVM_ProposedMethod3.py 100 01---------------"
+    python ML_SVM_ProposedMethod3.py ${PATH_RAW}
+
+    echo "------------ python ML_SVM_ProposedMethod2.py 300 012---------------"
+    python ML_SVM_ProposedMethod2.py ${PATH_RAW}
+
+    echo "------------ python ML_SVM_ProposedMethod.py 100 012---------------"
+    python ML_SVM_ProposedMethod.py ${PATH_RAW}
+
+    echo "------------ python ML_1dCNN_ProposedMethod4.py 300 01---------------"
+    python ML_1dCNN_ProposedMethod4.py ${PATH_RAW}
+
+    echo "------------ python ML_1dCNN_ProposedMethod4.py 100 01---------------"
+    python ML_1dCNN_ProposedMethod3.py ${PATH_RAW}
+
+    echo "------------ python ML_1dCNN_ProposedMethod4.py 300 012---------------"
+    python ML_1dCNN_ProposedMethod2.py ${PATH_RAW}
+
+    echo "------------ python ML_1dCNN_ProposedMethod4.py 100 012---------------"
+    python ML_1dCNN_ProposedMethod.py ${PATH_RAW}
+
+    echo "------------ python ML_SVM_RAWpt.py ---------------"
+    python ML_SVM_RAWpt.py ${PATH_RAW}
 
     #
     # PATH_MAL="${PATH_DATA}${dir}${image_method}/MAL5/"
